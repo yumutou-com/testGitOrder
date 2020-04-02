b7cf5aef6db6cdb5f4383839c7e85b03d03f1923
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-04-02 17:50:02 +0800

    修改模版

--- config/router.config.js
+++ config/router.config.js
@@ -11,6 +11,7 @@ export default [
     path: '/',
     component: '../layouts/AuthLayout',
     routes: [
+      { path: '/', redirect: '/dashboard' },
       { path: '/dashboard', component: './Dashboard' },
       {
         path: "/moduleName",
