f743545766eb6f4129e7d0454d26999a6fc5e14b
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-08-13 11:39:09 +0800

    测试

--- src/global.js
+++ src/global.js
@@ -55,26 +55,4 @@ if (process.env.NODE_ENV === 'production') {
       onClose: async () => {},
     });
   });
-} else if ('serviceWorker' in navigator) {
-  // unregister service worker
-  const { serviceWorker } = navigator;
-  if (serviceWorker.getRegistrations) {
-    serviceWorker.getRegistrations().then(sws => {
-      sws.forEach(sw => {
-        sw.unregister();
-      });
-    });
-  }
-  serviceWorker.getRegistration().then(sw => {
-    if (sw) sw.unregister();
-  });
-
-  // remove all caches
-  if (window.caches && window.caches.keys) {
-    caches.keys().then(keys => {
-      keys.forEach(key => {
-        caches.delete(key);
-      });
-    });
-  }
 }
