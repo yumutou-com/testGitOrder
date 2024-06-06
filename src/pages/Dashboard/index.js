79781bde6fe2cd3c8dcf6cf554b1d5f21724cc83
lan.yu <lan.yu@changhong.com>
2024-06-06 14:40:13 +0800

    [FIX]: 给board的btn添加点击事件，再增加一些内容

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Empty, Button } from 'antd';
+import { Empty, Button, Card } from 'antd';
 import { ProLayout, utils } from '@sei/suid';
 import { Link } from 'umi';
 
@@ -8,6 +8,10 @@ const { Content } = ProLayout;
 
 export default () => {
   const currUser = getContextUser();
+
+  const handleBtn = () => {
+    console.log('aaa');
+  }
 
   return (
     <ProLayout>
@@ -22,8 +26,12 @@ export default () => {
               }}
             >
               欢迎来到本模块进行开发
-              <Button type='primary' style={{marginRight: 8}}>btn1</Button>
+              <Button type='primary' style={{marginRight: 8}} onClick={handleBtn}>btn1</Button>
               <Button>btn2</Button>
+              <Card title='card-one' color='#8e8e8e'>
+                <p>1111111111111</p>
+                <p>2222222222222</p>
+              </Card>
             </div>
           }
         >
